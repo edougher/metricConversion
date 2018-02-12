600a080e93ef29cc6bb497c79ad1b26ba456e9bd
Erin Dougher <edougher@students.mchenry.edu>
2018-02-11 23:03:56 -0600

    Initial commit

---
+++ app/src/main/java/com/example/aaron/metricconversion/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.aaron.metricconversion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText inches = findViewById(R.id.inches);
+        final EditText feet = findViewById(R.id.feet);
+        final EditText pounds = findViewById(R.id.pounds);
+        final EditText faherenheit = findViewById(R.id.faherenheit);
+
+        final TextView cm = findViewById(R.id.cm);
+        final TextView meters = findViewById(R.id.meters);
+        final TextView kilos = findViewById(R.id.kilos);
+        final TextView celsius = findViewById(R.id.celcius);
+
+        Button convert = findViewById(R.id.convert_btn);
+
+        convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DecimalFormat currency = new DecimalFormat("###,###.##");
+
+                Double value1;
+                value1 = Double.valueOf(inches.getText().toString());
+                Double result1 = value1/0.39;
+                cm.setText(currency.format(result1));
+
+                Double value2;
+                value2 = Double.valueOf(feet.getText().toString());
+                Double result2 = value2/3.2808;
+                meters.setText(currency.format(result2));
+
+                Double value3;
+                value3 = Double.valueOf(pounds.getText().toString());
+                Double result3 = value3/2.2046;
+                kilos.setText(currency.format(result3));
+
+                Double value4;
+                value4 = Double.valueOf(faherenheit.getText().toString());
+                Double result4 = (value4 - 32.0) / 1.8;
+                celsius.setText(currency.format(result4));
+
+
+
+            }
+        });
+
+    }
+}
