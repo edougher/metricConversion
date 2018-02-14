11ce135142e16ec36bbb923976101d2979918278
Erin Dougher <edougher@students.mchenry.edu>
2018-02-14 12:40:23 -0600

    second

--- app/src/main/java/com/example/aaron/metricconversion/MainActivity.java
+++ app/src/main/java/com/example/aaron/metricconversion/MainActivity.java
@@ -28,37 +28,63 @@ public class MainActivity extends AppCompatActivity {
         final TextView celsius = findViewById(R.id.celcius);
 
         Button convert = findViewById(R.id.convert_btn);
+        Button convert2 = findViewById(R.id.convert_btn2);
+        Button convert3 = findViewById(R.id.convert_btn3);
+        Button convert4 = findViewById(R.id.convert_btn4);
+
+        final DecimalFormat currency = new DecimalFormat("###,###.##");
 
         convert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                DecimalFormat currency = new DecimalFormat("###,###.##");
-
-                Double value1;
+                Double value1 ;
                 value1 = Double.valueOf(inches.getText().toString());
-                Double result1 = value1/0.39;
+                Double result1 = value1 / 0.39;
                 cm.setText(currency.format(result1));
 
-                Double value2;
+            }
+        });
+
+        convert2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Double value2 ;
                 value2 = Double.valueOf(feet.getText().toString());
-                Double result2 = value2/3.2808;
+                Double result2 = value2 / 3.2808;
                 meters.setText(currency.format(result2));
+
+            }
+        });
+
+        convert3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
                 Double value3;
                 value3 = Double.valueOf(pounds.getText().toString());
-                Double result3 = value3/2.2046;
+                Double result3 = value3 / 2.2046;
                 kilos.setText(currency.format(result3));
+
+
+            }
+        });
+
+        convert4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
                 Double value4;
                 value4 = Double.valueOf(faherenheit.getText().toString());
                 Double result4 = (value4 - 32.0) / 1.8;
                 celsius.setText(currency.format(result4));
 
-
-
             }
         });
 
+
     }
+
 }
+
